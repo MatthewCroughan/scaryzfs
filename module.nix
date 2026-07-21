9f6245e320a0359b52403b081548b7cf1e87a631
matthewcroughan <matt@croughan.sh>
2026-07-21 13:06:28 +0100

    wip

--- module.nix
+++ module.nix
@@ -64,7 +64,7 @@ in {
         inherit configFile kernel;
       } {
         kernelModuleAttribute = "scaryzfs";
-#        kernelCompatible = kernel: kernel.kernelOlder "9999999999.9999999999";
+        kernel = kernel;
         kernelMinSupportedMajorMinor = kernel.version;
         kernelMaxSupportedMajorMinor = kernel.version;
         version = "scary";
